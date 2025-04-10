05a4fff8516fc368fd5bfbd7f318864784a9d150
shivangi286 <shivangi.mahajan@teleperformance.com>
2025-04-10 22:53:35 +0530

    Added main view for chat bot

---
+++ mainView.js
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+      window.ymConfig = {"bot":"x1738070939993","host":"https://r0.cloud.yellow.ai"};
+    (function() {
+        var w = window,
+            ic = w.YellowMessenger;
+        if ("function" === typeof ic) ic("reattach_activator"), ic("update", ymConfig);
+        else {
+            var d = document,
+                i = function() {
+                    i.c(arguments)
+                };
+            function l() {
+                var e = d.createElement("script");
+                e.type = "text/javascript", e.async = !0, e.src = "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";
+                var t = d.getElementsByTagName("script")[0];
+                t.parentNode.insertBefore(e, t)
+            }
+            i.q = [], i.c = function(e) {
+                i.q.push(e)
+            }, w.YellowMessenger = i, w.attachEvent ? w.attachEvent("onload", l) : w.addEventListener("load", l, !1)}
+    })();
+    </script>
